e3e6016938e7906aa97ba030daae33bcf4f23ddb
defremov <defremov@gmail.com>
2021-06-17 18:57:26 +0300

    .

--- Task2.py
+++ Task2.py
@@ -1,6 +1,17 @@
-#
-sec = int(input("Введите время в секундах: "))
-h = sec // 3600
-m = (sec - h * 3600) // 60
-s = sec % 60
-print(f"Форматированное время: {h:02d}:{m:02d}:{s:02d}")
+print("Введите элементы списка. Для окончания ввода вместо очередного элемента сразу же нажмите <ENTER>.")
+a = []
+while True:
+    s = input()
+    if s == "":
+        break
+    else:
+        a.append(s)
+print("До перестановок: ")
+for i in a:
+    print(i, end=" ")
+print()
+for i in range(0, len(a) // 2):
+    (a[2 * i], a[2 * i + 1]) = (a[2 * i + 1], a[2 * i])
+print("После перестановок: ")
+for i in a:
+    print(i, end=" ")
--- Task4.py
+++ Task4.py
@@ -1,8 +1,3 @@
-n = int(input("Введите целое положительное число: "))
-dmax = 0
-while (n > 0):
-    d = n % 10
-    if d > dmax:
-        dmax = d
-    n = n // 10
-print(f"Набольшая цифра в этом числе: {dmax}")
+a = input("Введите строку: ").split()
+for j, b in enumerate(a):
+    print(f"Слово № {j+1}: '{b[:10]}'")
