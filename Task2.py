728579bcc6fc10238136307a0fa85181e34e5624
defremov <defremov@gmail.com>
2021-06-22 17:26:12 +0300

    .

--- Task2.py
+++ Task2.py
@@ -1,17 +1,5 @@
-print("Введите элементы списка. Для окончания ввода вместо очередного элемента сразу же нажмите <ENTER>.")
-a = []
-while True:
-    s = input()
-    if s == "":
-        break
-    else:
-        a.append(s)
-print("До перестановок: ")
-for i in a:
-    print(i, end=" ")
-print()
-for i in range(0, len(a) // 2):
-    (a[2 * i], a[2 * i + 1]) = (a[2 * i + 1], a[2 * i])
-print("После перестановок: ")
-for i in a:
-    print(i, end=" ")
+def dataout(name, secondname, year, city, email, phone):
+    return f"Имя: {name}, Фамилия: {secondname}, Год рождения: {year}, Город проживания: {city}, email: {email}, Телефон: {phone}"
+
+
+print(dataout(name="John", secondname="Doe", year="666 AD", city = "Inferno", email = "me@hell.com", phone = "+666 666 666"))
