7359f594091a20cb2a453110f1d78207293634f8
defremov <defremov@gmail.com>
2021-06-22 17:27:55 +0300

    .

--- Task4.py
+++ Task4.py
@@ -1,3 +1,19 @@
-a = input("Введите строку: ").split()
-for j, b in enumerate(a):
-    print(f"Слово № {j+1}: '{b[:10]}'")
+def my_func(x, y):
+    try:
+        xx = float(x)
+        yy = int(y)
+    except ValueError:
+        return "ОШИБКА. Ошибка преобразования типов"
+    if xx <= 0:
+        return "ОШИБКА. Основание должно быть положительным!"
+    if yy >= 0:
+        return "ОШИБКА. Степень должна быть целой и отрицательной!"
+    temp = 1
+    for i in range(-yy):
+        temp = temp * xx
+    return 1 / temp
+
+
+x = input("Введите основание: ")
+y = input("Введите целую отрицательную степень: ")
+print(f"Результат возведения в степень: {my_func(x,y)}")
